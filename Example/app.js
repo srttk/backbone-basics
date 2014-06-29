aa01de1c02b3848fdef049e38928b7760377b8fd
sarath.hacks@gmail.com <sarath.hacks@gmail.com>
2014-06-29 16:05:14 +0530

    Updated

--- Example/app.js
+++ Example/app.js
@@ -1,6 +1,8 @@
 define(['jquery','underscore','Backbone','views/headerview','views/homeview','views/aboutview','views/notfoundview'],function($,_,Backbone,HeaderView,HomeView,AboutView,NFView){
     var App=Backbone.Router.extend({
     	initialize:function(){
+            console.log('This site is Created and maintained by Sarath');
+            console.log('Visit http://saratonite.github.io  For more information.');
     		this.header=new HeaderView();
     	},
     	routes:{
